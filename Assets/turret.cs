ce55a3f5059c71f04191959360b72a224de74e80
Arnas Puskunigis <arnaspusk204@gmail.com>
2025-03-06 13:31:29 +0000

    added a turret model and a rocket model

--- Assets/turret.cs
+++ Assets/turret.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem.Controls;
+using UnityEngine.Rendering;
 
 public class turret : MonoBehaviour
 {
@@ -10,11 +12,11 @@ public class turret : MonoBehaviour
     public GameObject target;
     public GameObject turretGun;
     public GameObject bullet;
-    public Transform bulletSpawnPoint;
+    public Transform[] bulletSpawnPoints;
     public float bulletForce;
     public float shotCooldown;
     public float timeElapsed;
-    
+    public GameObject destroy;
 
     // Start is called before the first frame update
     void Start()
@@ -29,7 +31,8 @@ public class turret : MonoBehaviour
         timeElapsed += Time.deltaTime;
         if (timeElapsed > shotCooldown )
         {
-            Instantiate(bullet, bulletSpawnPoint);
+            int random = Random.Range(0, bulletSpawnPoints.Length);
+            Instantiate(bullet, bulletSpawnPoints[random].position, bulletSpawnPoints[random].rotation * Quaternion.Euler(0,-90,0), bulletSpawnPoints[random]);
             timeElapsed = 0f;
         }
     }
