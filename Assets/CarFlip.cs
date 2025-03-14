f6cef3e063a47cb8b121001d6e72541d26d9373f
Arnas Puskunigis <arnaspusk204@gmail.com>
2025-03-14 12:04:30 +0000

    added an unflip function, now when you press F you are unflipped

---
+++ Assets/CarFlip.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarFlip : MonoBehaviour
+{
+
+    public GameObject theCar;
+    public loadCar carLoader;
+    public bool carFound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        carLoader = GameObject.Find("CarLoader").GetComponent<loadCar>();
+        if (carLoader.currentCar != null)
+        {
+            theCar = carLoader.currentCar;
+            carFound = true;
+        }
+        else
+        {
+            carFound = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (carFound)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                theCar.transform.rotation = Quaternion.Euler(theCar.transform.rotation.x, theCar.transform.rotation.y, 0);
+            }
+        }
+        else if (!carFound && carLoader.currentCar != null)
+        {
+            theCar = carLoader.currentCar;
+            carFound = true;
+        }
+    }
+}
