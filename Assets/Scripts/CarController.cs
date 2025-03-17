9a72439ead8aec32a9da7926e4207fa000798ca9
Arnas Puskunigis <arnaspusk204@gmail.com>
2025-03-17 10:10:13 +0000

    testing new car controls

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,284 +1,82 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    private Rigidbody playerRB;
-    public WheelColliders colliders;
-    public WheelMeshes wheelMeshes;
-    public WheelParticles wheelParticles;
-    public float gasInput;
-    public float brakeInput;
-    public float steeringInput;
-    public GameObject smokePrefab;
-    public float motorPower;
-    public float brakePower;
-    public float slipAngle;
-    public float speed;
-    public AnimationCurve steeringCurve;
+    private float horizontalInput, verticalInput;
+    private float currentSteerAngle, currentbreakForce;
+    private bool isBreaking;
 
-    public Mybutton gasPedal;
-    public Mybutton brakePedal;
-    public Mybutton leftButton;
-    public Mybutton rightButton;
+    // Settings
+    [SerializeField] private float motorForce, breakForce, maxSteerAngle;
 
-    public bool isDrifting1;
-    public bool isDrifting2;
-    public bool isDrifting3;
-    public bool isDrifting4;
+    // Wheel Colliders
+    [SerializeField] private WheelCollider frontLeftWheelCollider, frontRightWheelCollider;
+    [SerializeField] private WheelCollider rearLeftWheelCollider, rearRightWheelCollider;
 
-    public float driftCounter;
-    public float driftPoints;
+    // Wheels
+    [SerializeField] private Transform frontLeftWheelTransform, frontRightWheelTransform;
+    [SerializeField] private Transform rearLeftWheelTransform, rearRightWheelTransform;
 
-    public exp expManager;
-    public multiplierManager multManager;
-    public driftTextManager driftTextManager;
-
-    public EngineAudioScript carEngineAudioScript;
-
-    public bool EnableVR;
-
-    // Start is called before the first frame update
-    void Start()
+    private void FixedUpdate()
     {
-        playerRB = gameObject.GetComponent<Rigidbody>();
-        carEngineAudioScript = transform.gameObject.GetComponent<EngineAudioScript>();
-        InstantiateSmoke();
-        //wheelParticles.FRWheel.Play();
-        //wheelParticles.FLWheel.Play();
-        wheelParticles.RRWheel.Play();
-        wheelParticles.RLWheel.Play();
+        GetInput();
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
     }
-     
-    void InstantiateSmoke()
+
+    private void GetInput()
     {
-        wheelParticles.FRWheel = Instantiate(smokePrefab, colliders.FRWheel.transform.position-Vector3.up*colliders.FRWheel.radius, Quaternion.identity, colliders.FRWheel.transform)
-            .GetComponent<ParticleSystem>();
-        wheelParticles.FLWheel = Instantiate(smokePrefab, colliders.FLWheel.transform.position- Vector3.up * colliders.FRWheel.radius, Quaternion.identity, colliders.FLWheel.transform)
-            .GetComponent<ParticleSystem>();
-        wheelParticles.RRWheel = Instantiate(smokePrefab, colliders.RRWheel.transform.position- Vector3.up * colliders.FRWheel.radius, Quaternion.identity, colliders.RRWheel.transform)
-            .GetComponent<ParticleSystem>();
-        wheelParticles.RLWheel = Instantiate(smokePrefab, colliders.RLWheel.transform.position- Vector3.up * colliders.FRWheel.radius, Quaternion.identity, colliders.RLWheel.transform)
-            .GetComponent<ParticleSystem>();
-    }
-    // Update is called once per frame
+        // Steering Input
+        horizontalInput = Input.GetAxis("Horizontal");
 
-    void Update()
+        // Acceleration Input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // Breaking Input
+        isBreaking = Input.GetKey(KeyCode.Space);
+    }
+
+    private void HandleMotor()
     {
-        speed = playerRB.velocity.magnitude;
-        CheckInput();
-        ApplyMotor();
-        ApplySteering();
-        ApplyBrake();
-        CheckParticles();
-        ApplyWheelPositions();
+        frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
+        frontRightWheelCollider.motorTorque = verticalInput * motorForce;
+        currentbreakForce = isBreaking ? breakForce : 0f;
+        ApplyBreaking();
     }
 
-    void CheckInput()
+    private void ApplyBreaking()
     {
-        if (EnableVR)
-        {
-            if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
-            {
-                gasInput = 1;
-            }
-            else if (OVRInput.Get(OVRInput.Button.PrimaryHandTrigger))
-            {
-                gasInput = -1;
-            }
-            else
-            {
-                gasInput = 0;
-            }
-
-            steeringInput = Input.GetAxis("Horizontal");
-            slipAngle = Vector3.Angle(transform.forward, playerRB.velocity - transform.forward);
-
-            float movingDirection = Vector3.Dot(transform.forward, playerRB.velocity);
-            if (movingDirection < -0.5f && gasInput > 0)
-            {
-                brakeInput = Mathf.Abs(gasInput);
-            }
-            else if (movingDirection > 0.5f && gasInput < 0)
-            {
-                brakeInput = Mathf.Abs(gasInput);
-            }
-            else
-            {
-                brakeInput = 0;
-            }
-        }
-        else
-        {
-            gasInput = Input.GetAxis("Vertical");
-            steeringInput = Input.GetAxis("Horizontal");
-            slipAngle = Vector3.Angle(transform.forward, playerRB.velocity - transform.forward);
-
-            float movingDirection = Vector3.Dot(transform.forward, playerRB.velocity);
-            if (movingDirection < -0.5f && gasInput > 0)
-            {
-                brakeInput = Mathf.Abs(gasInput);
-            }
-            else if (movingDirection > 0.5f && gasInput < 0)
-            {
-                brakeInput = Mathf.Abs(gasInput);
-            }
-            else
-            {
-                brakeInput = 0;
-            }
-        }
-
+        frontRightWheelCollider.brakeTorque = currentbreakForce;
+        frontLeftWheelCollider.brakeTorque = currentbreakForce;
+        rearLeftWheelCollider.brakeTorque = currentbreakForce;
+        rearRightWheelCollider.brakeTorque = currentbreakForce;
     }
-    void ApplyBrake()
+
+    private void HandleSteering()
     {
-        colliders.FRWheel.brakeTorque = brakeInput * brakePower* 0.7f ;
-        colliders.FLWheel.brakeTorque = brakeInput * brakePower * 0.7f;
-
-        colliders.RRWheel.brakeTorque = brakeInput * brakePower * 0.3f;
-        colliders.RLWheel.brakeTorque = brakeInput * brakePower *0.3f;
+        currentSteerAngle = maxSteerAngle * horizontalInput;
+        frontLeftWheelCollider.steerAngle = currentSteerAngle;
+        frontRightWheelCollider.steerAngle = currentSteerAngle;
     }
-    void ApplyMotor() {
 
-        colliders.RRWheel.motorTorque = motorPower * gasInput;
-        colliders.RLWheel.motorTorque = motorPower * gasInput;
-    }
-    void ApplySteering()
+    private void UpdateWheels()
     {
-
-        float steeringAngle = steeringInput*steeringCurve.Evaluate(speed);
-        if (slipAngle < 120f)
-        {
-            steeringAngle += Vector3.SignedAngle(transform.forward, playerRB.velocity + transform.forward, Vector3.up);
-        }
-        steeringAngle = Mathf.Clamp(steeringAngle, -90f, 90f);
-        colliders.FRWheel.steerAngle = steeringAngle;
-        colliders.FLWheel.steerAngle = steeringAngle;
+        UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform);
+        UpdateSingleWheel(frontRightWheelCollider, frontRightWheelTransform);
+        UpdateSingleWheel(rearRightWheelCollider, rearRightWheelTransform);
+        UpdateSingleWheel(rearLeftWheelCollider, rearLeftWheelTransform);
     }
 
-    void ApplyWheelPositions()
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
     {
-        UpdateWheel(colliders.FRWheel, wheelMeshes.FRWheel);
-        UpdateWheel(colliders.FLWheel, wheelMeshes.FLWheel);
-        UpdateWheel(colliders.RRWheel, wheelMeshes.RRWheel);
-        UpdateWheel(colliders.RLWheel, wheelMeshes.RLWheel);
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.rotation = rot;
+        wheelTransform.position = pos;
     }
-    void CheckParticles() {
-        WheelHit[] wheelHits = new WheelHit[4];
-        colliders.FRWheel.GetGroundHit(out wheelHits[0]);
-        colliders.FLWheel.GetGroundHit(out wheelHits[1]);
-
-        colliders.RRWheel.GetGroundHit(out wheelHits[2]);
-        colliders.RLWheel.GetGroundHit(out wheelHits[3]);
-
-        float slipAllowance = 1.4f;
-        
-        //if ((Mathf.Abs(wheelHits[0].sidewaysSlip) + Mathf.Abs(wheelHits[0].forwardSlip) > slipAllowance)){
-        //    wheelParticles.FRWheel.emissionRate = 100;
-        //    isDrifting1 = true;
-        //}
-        //else
-        //{
-        //    wheelParticles.FRWheel.emissionRate = 0;
-        //    isDrifting1 = false;
-        //}
-        //if ((Mathf.Abs(wheelHits[1].sidewaysSlip) + Mathf.Abs(wheelHits[1].forwardSlip) > slipAllowance)){
-        //    wheelParticles.FLWheel.emissionRate = 100;
-        //    isDrifting2 = true;
-        //}
-        //else
-        //{
-        //    wheelParticles.FLWheel.emissionRate = 0;
-        //    isDrifting2 = false;
-        //}
-        if ((Mathf.Abs(wheelHits[2].sidewaysSlip) + Mathf.Abs(wheelHits[2].forwardSlip) > slipAllowance)){
-            wheelParticles.RRWheel.emissionRate = 100;
-            isDrifting3 = true;
-        }
-        else
-        {
-            wheelParticles.RRWheel.emissionRate = 0;
-            isDrifting3 = false;
-        }
-        if ((Mathf.Abs(wheelHits[3].sidewaysSlip) + Mathf.Abs(wheelHits[3].forwardSlip) > slipAllowance)){
-            wheelParticles.RLWheel.emissionRate = 100;
-            isDrifting4 = true;
-        }
-        else
-        {
-            wheelParticles.RLWheel.emissionRate = 0;
-            isDrifting4 = false;
-        }
-
-        checkIfDrifting();
-
-    }
-
-    void checkIfDrifting()
-    {
-        if (isDrifting1 || isDrifting2 || isDrifting3 || isDrifting4)
-        {
-            carEngineAudioScript.drifting = true;
-            if (driftCounter > 1)
-            {
-                driftCounter += Time.deltaTime * 2;
-            }
-            else if (driftCounter > 5)
-            {
-                driftCounter += Time.deltaTime * 4;
-            }
-            else if (driftCounter > 15)
-            {
-                driftCounter += Time.deltaTime * 8;
-            }
-            else
-            {
-                driftCounter += Time.deltaTime;
-            }
-            driftTextManager.updateText(driftCounter);
-        }
-        else
-        {
-            carEngineAudioScript.drifting = false;
-            driftPoints += driftCounter;
-            expManager.addDriftPoints(driftCounter);
-            multManager.getDrift(driftCounter);
-            driftCounter = 0;
-            driftTextManager.stopText();
-        }
-    }
-
-    void UpdateWheel(WheelCollider coll, MeshRenderer wheelMesh)
-    {
-        Quaternion quat;
-        Vector3 position;
-        coll.GetWorldPose(out position, out quat);
-        wheelMesh.transform.position = position;
-        wheelMesh.transform.rotation = quat;
-    }
-}
-[System.Serializable]
-public class WheelColliders
-{
-    public WheelCollider FRWheel;
-    public WheelCollider FLWheel;
-    public WheelCollider RRWheel;
-    public WheelCollider RLWheel;
-}
-[System.Serializable]
-public class WheelMeshes
-{
-    public MeshRenderer FRWheel;
-    public MeshRenderer FLWheel;
-    public MeshRenderer RRWheel;
-    public MeshRenderer RLWheel;
-}
-[System.Serializable]
-public class WheelParticles{
-    public ParticleSystem FRWheel;
-    public ParticleSystem FLWheel;
-    public ParticleSystem RRWheel;
-    public ParticleSystem RLWheel;
-
 }
